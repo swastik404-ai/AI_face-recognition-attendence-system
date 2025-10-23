429057ad720844d16977a162a88cec7de6bcd192
swastik404-ai <r4m4n666666@gmail.com>
2025-10-23 18:46:19 +0530

    urls.py

--- Project101/urls.py
+++ Project101/urls.py
@@ -18,9 +18,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app1.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Add static files serving
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
